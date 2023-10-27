7c422b4adb0cee87bf4ea361a6da608c90fa69a0
Joel <58995538+jtpotato@users.noreply.github.com>
2023-10-27 18:44:13 +1100

    add victorian data

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ print(f"COVID R0: {covid_r0}")
 simulate(population=8000000000, initial_cases=282, simulation_days=305, covid_r0=covid_r0, immune_days=200, save_file=True, filename="data/no_intervention.csv")
 
 # Find Re of COVID-19 using data from Victorian restrictions (mask wearing + lockdown) between the dates of 1 August and 1 September 2020.
-covid_re_mask = find_r0(initial_cases=10931, final_cases=19138, population=6681000, days=31, iterations=10000, mode="cumulative")
+covid_re_mask = find_r0(initial_cases=5919, final_cases=2519, population=6681000, days=31, iterations=10000, mode="active")
 print(f"COVID Re (with Victorian Stage 4 - 2020 Restrictions): {covid_re_mask}")
 
-# Simualte the spread of COVID in the world from 282 cases on March 1st, if the world followed Victorian restrictions during that timeframe.
+# Simulate the spread of COVID in the world from 282 cases on March 1st, if the world followed Victorian restrictions during that timeframe.
 simulate(population=8000000000, initial_cases=282, simulation_days=305, covid_r0=covid_re_mask, immune_days=200, save_file=True, filename="data/stage_4-2020.csv")
+
+# Find Re of COVID-19 using data from V
