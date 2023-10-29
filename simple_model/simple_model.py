9ac31e60d1000cb127823ef104739595ad6aa0a9
Joel <58995538+jtpotato@users.noreply.github.com>
2023-10-30 09:16:31 +1100

    made a mistake

--- simple_model/simple_model.py
+++ simple_model/simple_model.py
@@ -2,12 +2,14 @@ initial_cases = 7769783
 final_cases = 12741386
 days = 28 # Days between 8-6-2020 and 6-7-2020
 
-# Calculate alpha.
+# Initial value of alpha
 alpha = 0.03
 
+# Iterate 1000 times to approximate alpha
 for i in range(1000):
     cases = initial_cases
 
+    # Run simulation
     for i in range(days):
         cases = cases + int(cases * alpha)
 
@@ -24,7 +26,6 @@ print(f"Alpha Value: {alpha}") # Alpha Value: 0.017800000000000073
 # Calculate cases on December 31st 2020
 cases = final_cases # Cases from 6-7-2020
 days = 178 # Days between 6-7-2020 and 31-12-2020
-alpha = 0.01782175760401952 # Alpha Value as previously calculated 
 
 with open("simple_model.csv", "w") as f:
     for i in range(days):
